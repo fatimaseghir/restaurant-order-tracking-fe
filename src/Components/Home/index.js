ad7e83590512d471e61d8ef18e4ad51fa4b92d89
Fatima <fzms90@gmail.com>
2022-12-12 12:09:10 +0000

    logos/images/ and home component

---
+++ src/Components/Home/index.js
@@ -0,0 +1,14 @@
+import img1 from '../../images/img1.jpg';
+
+
+function Home() {
+
+    return (
+        <div className="bg-image -flex justify-content-center align-items-center">
+             <img className="bg-image h-80" src={img1} alt="image" ></img>
+
+        </div>
+    );
+}
+
+export default Home;
