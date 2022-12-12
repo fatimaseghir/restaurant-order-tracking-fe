e3c53c146a107a29530c538f7e5dafed81fdd8de
Fatima <fzms90@gmail.com>
2022-12-12 14:57:52 +0000

    home component

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -6,18 +6,17 @@ import './style.css';
 function Home() {
 
     return (
-        <div className="home container d-flex justify-content-center align-items-center">
-
-                <img className="img1 bg-image vh-100 min-vw-100 position-relative" src={img1} alt="image" ></img>
-            <div className="position-absolute text-center">
-                    <img className="logo " src={flame} alt="logo"></img>
-                    <h1 className="text-light">STANLEYS’ STEAKS</h1>
-                    <hr className="text-light"></hr>
-                    <h3 className="text-light">lovingly reared cattle from our farm to you table</h3>
+        <div className="home" >
+            <img className="img1" src={img1} ></img>
+            <div className="container">
+                    <img className="logo" src={flame} alt="logo"></img>
+                    <h1 className="">STANLEYS’ STEAKS</h1>
+                    <hr className=""></hr>
+                    <h3 className="">lovingly reared cattle from our farm to you table</h3>
             </div>
-
         </div>
     );
 }
 
 export default Home;
+
---
+++ src/Components/NavBar/index.js
@@ -0,0 +1,20 @@
+import './style.css';
+import menu from '../../icons/flame.png';
+import events  from '../../icons/flame.png';
+import location from '../../icons/flame.png';
+import farm from '../../icons/flame.png';
+
+
+function NavBar() {
+
+    return (
+            <div className="nav-bar text-end pb-4 me-5">
+                <a><img className="logo" src="" alt="image" ></img></a>
+                <a><img className="logo" src="" alt="image" ></img></a>
+                <a><img className="logo" src="" alt="image" ></img></a>
+                <a><img className="logo" src="" alt="image" ></img></a>
+            </div>
+    );
+}
+
+export default NavBar;
