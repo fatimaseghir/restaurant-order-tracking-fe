dd5cbefa18ccd385f71730151efcc502d02c3ac8
Fatima <fzms90@gmail.com>
2022-12-12 13:29:31 +0000

    font

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,11 +1,20 @@
+import flame from '../../icons/flame.png';
 import img1 from '../../images/img1.jpg';
+import './style.css';
 
 
 function Home() {
 
     return (
-        <div className="bg-image -flex justify-content-center align-items-center">
-             <img className="bg-image h-80" src={img1} alt="image" ></img>
+        <div className="home container d-flex justify-content-center align-items-center">
+
+                <img className="img1 bg-image vh-100 min-vw-100 position-relative" src={img1} alt="image" ></img>
+            <div className="position-absolute text-center">
+                    <img className="logo " src={flame} alt="logo"></img>
+                    <h1 className="text-light">STANLEYS’ STEAKS</h1>
+                    <hr className="text-light"></hr>
+                    <h3 className="text-light">lovingly reared cattle from our farm to you table</h3>
+            </div>
 
         </div>
     );
