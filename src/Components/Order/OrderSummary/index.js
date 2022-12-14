3e1ab958698e7a35d19c6f38e08bcf35d08f8b69
owen <may.owend@gmail.com>
2022-12-14 12:33:02 +0000

    all css changes made to order summary modal. design is responsive for mobile and md and lg monitor sizes and buttons have been redesigned

--- src/Components/Order/OrderSummary/index.js
+++ src/Components/Order/OrderSummary/index.js
@@ -10,7 +10,7 @@ function OrderSummary() {
 
     return (
         <>
-            <div className="modal fade" id="orderSummaryModal" tabIndex="-1" aria-labelledby=""
+            <div className="modal modal-xl fade" id="orderSummaryModal" tabIndex="-1" aria-labelledby=""
                  aria-hidden="true">
                 <div className="modal-dialog">
                     <div className="modal-content">
@@ -20,38 +20,61 @@ function OrderSummary() {
                                     aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
-                            <form name="orderSummaryForm">
+
+                            <div className="container-fluid">
+                                <div className="row">
+                                    <div className="col-7 fw-bold">Item</div>
+                                    <div className="col-3 fw-bold text-center">Amount</div>
+                                    <div className="col-1 fw-bold">Price</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-7">Cheeseburger</div>
+                                    <div className="col-3 text-center">2</div>
+                                    <div className="col-1">£22.98</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-7">Cheeseburger</div>
+                                    <div className="col-3 text-center">2</div>
+                                    <div className="col-1">£22.98</div>
+                                </div>
+                                <div className="row border-bottom border-1 border-light pb-2 mb-2">
+                                    <div className="col-7">Cheeseburger</div>
+                                    <div className="col-3 text-center">2</div>
+                                    <div className="col-1">£22.98</div>
+                                </div>
+                                <div className="row mb-3">
+                                    <div className="col-7"></div>
+                                    <div className="col-3 fw-bold text-end">Total:</div>
+                                    <div className="col-1">£108.76</div>
+                                </div>
+                            </div>
+
+                            <form name="orderSummaryForm ">
 
                                 <label htmlFor="first-name">First name</label>
-                                <input id="first-name" name="first-name" className="submit-contact form-control"
+                                <input id="first-name" name="first-name" className="submit-contact form-control w-50"
                                        type="text" />
-                                <div id="firstNameError" className="alert hidden formItem_alert"></div>
 
                                 <label htmlFor="last-name">Last name</label>
-                                <input id="last-name" name="first-name" className="submit-contact form-control"
+                                <input id="last-name" name="first-name" className="submit-contact form-control w-50"
                                        type="text" />
-                                <div id="firstNameError" className="alert hidden formItem_alert"></div>
 
                                 <label htmlFor="email">Email</label>
-                                <input id="email" name="email" className="submit-contact form-control"
+                                <input id="email" name="email" className="submit-contact form-control w-75"
                                        type="text" />
-                                <div id="emailError" className="alert hidden formItem_alert"></div>
 
                                 <label htmlFor="address">Address</label>
-                                <input id="address" name="address" className="submit-contact form-control"
-                                       type="text" />
-                                <div id="addressError" className="alert hidden formItem_alert"></div>
+                                <textarea id="address" name="address" className="submit-contact form-control w-75 h-25"
+                                       type="text" rows="2" />
 
                                 <label htmlFor="postcode">Postcode</label>
-                                <input id="postcode" name="postcode" className="submit-contact form-control"
+                                <input id="postcode" name="postcode" className="submit-contact form-control w-25 mb-4"
                                        type="text" />
-                                <div id="postcodeError" className="alert hidden formItem_alert"></div>
-
 
                                 <div id="orderSummaryFooter" className="modal-footer justify-content-between">
-                                    <button type="button" className="fw-bold bg-dark text-light border border-3 border-light" data-bs-dismiss="modal">Cancel Order</button>
-                                    <button type="button" className="fw-bold bg-dark text-light border border-3 border-light" data-bs-dismiss="modal">Modify</button>
-                                    <button type="submit" className="fw-bold bg-dark text-light border border-3 border-light" data-bs-dismiss="modal">Complete</button>
+                                    <button type="button" className="border border-3 border-light" data-bs-dismiss="modal">CANCEL</button>
+                                    <button type="button" className="border border-3 border-light" data-bs-dismiss="modal">MODIFY</button>
+                                    <button type="submit" className="border border-3 border-light" data-bs-dismiss="modal">COMPLETE</button>
                                 </div>
 
                             </form>
